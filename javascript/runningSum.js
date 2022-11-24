8c87467c1ed35e29a99ab9e7243881561a843e1b
Wilson Kinyua <wilsonkinyuam@gmail.com>
2022-11-25 00:25:46 +0300

    Running Sum of 1d Array solution

--- javascript/runningSum.js
+++ javascript/runningSum.js
@@ -1,12 +1,20 @@
+/**
+ * https://leetcode.com/problems/running-sum-of-1d-array/
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 var runningSum = function (nums) {
-  let sum = [nums.length];
-  sum[0] = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    sum[i] = sum[i - 1] + nums[i];
+  //   let sum = [nums.length];
+  // sum[0] = nums[0];
+  // for (let i = 1; i < nums.length; i++) {
+  //   sum[i] = sum[i - 1] + nums[i];
+  // }
+  // return sum;
+  let sum = 0;
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    sum = sum + nums[i];
+    result.push(sum);
   }
-  return sum;
+  return result;
 };
-
-console.log(runningSum([1, 2, 3, 4]));
-console.log(runningSum([1, 1, 1, 1, 1]));
-console.log(runningSum([3, 1, 2, 10, 1]));
